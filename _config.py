2df7c569ae5ecc42da21887f11e8529f014c032a
Prtfw <shang.madeleine@gmail.com>
2018-02-15 22:28:45 -0800

    added comments and doc strings

--- _config.py
+++ _config.py
@@ -2,26 +2,29 @@ from gensim.models.keyedvectors import KeyedVectors
 import numpy as np
 
 PATHS = dict(
-CSVPATH= "./data_cleaned.csv",
-DEFAULT_EMBEDDING_PATH = "./GoogleNews-vectors-negative300.bin.gz",
-EMB_PATH="./saved_outputs/custom_trained_embeddings1"
+CSVPATH= "./data_cleaned.csv", # path for load_df
+DEFAULT_EMBEDDING_PATH = "./GoogleNews-vectors-negative300.bin.gz", # path for pre-trained embeddings
+EMB_PATH="./saved_outputs/custom_trained_embeddings1" # path to save custom trained embeddings
 )
 
 EMBEDDINGS = dict(
 EMBEDDING_DIM = 300,
 MAX_SEQUENCE_LENGTH = 1000,
 VALIDATION_SPLIT=.4,
-VOCAB_COL_NAMES = 'text',
+VOCAB_COL_NAMES = 'text', # column name for text (input) feature
 DEFAULT_EMBEDDING_INDEX = KeyedVectors.load_word2vec_format(PATHS['DEFAULT_EMBEDDING_PATH'], binary=True)
+# dict to look up off the shelf embeddings
 )
 
 
 EMB_TRAIN_TASK = {'dfcolumn': 'tags', 'is_categorical': True, 'min_count': 20 }
+# dfcolumn = label for embdding training task, is_categorical: are labels discrete, min_count: remove rare labels, set to 1 if continuous
 
-INIT_COST_WEIGHTS = np.ones((3,3))
-INIT_COST_WEIGHTS[1,0]=5
+
+INIT_COST_WEIGHTS = np.ones((3,3)) # intialize asymetrical penalty array for custom loss function (3 classes by default)
+INIT_COST_WEIGHTS[1,0]=5 # penalty for misclassifying true class 2 as class 1
 INIT_COST_WEIGHTS[2,0]=15
 INIT_COST_WEIGHTS[2,1]=1
 
-INIT_COST_WEIGHTS
-LABEL_COL = 'tier'
+
+LABEL_COL = 'tier' # target/label column for 2nd stage task (for 2nd stage model after training embedding)
--- data_processing/data_pipeline.py
+++ data_processing/data_pipeline.py
@@ -10,6 +10,15 @@ from nltk.tokenize import RegexpTokenizer
 
 
 def load_df(path):
+    '''
+    This loads to df from a given csv path
+
+    Params
+    ----------
+    path: string, path to csv file
+    ----------
+    return: data frame
+    '''
     EMB_TRAIN_TASK_LABELS = _config.EMB_TRAIN_TASK['dfcolumn']
     df = pd.read_csv(_config.PATHS['CSVPATH'])
     hasTags = df[df[EMB_TRAIN_TASK_LABELS].notnull()]
@@ -21,12 +30,21 @@ def load_df(path):
     summary_stats = tags.values.sum(axis=0)
     ALL_TAGS = list(tags)
     KEEP_TAGS = [ALL_TAGS[ind] for ind, count in enumerate(summary_stats) if count > _config.EMB_TRAIN_TASK['min_count']]
-    # print(list(KEEP_TAGS))
     tags= tags[KEEP_TAGS]
     df = pd.concat([hasTags, tags], axis=1)
     return df, KEEP_TAGS
 
 def get_vocab (df, col):
+    '''
+    This generate vocab
+
+    Params
+    ----------
+    df: dataframe
+    col: column to pull vocab from
+    ----------
+    return: array
+    '''
     tokenizer = RegexpTokenizer(r'\w+')
     NEW_COL_NAME = str(col)+'_tokens'
     df[col] = df[col].astype(str)
@@ -40,6 +58,25 @@ def get_vocab (df, col):
     return VOCAB
 
 def process_embeddings(df, col, VOCAB, baseEmbeddingDict, isMultilabel, labelCols):
+    '''
+    This looks up the embeddings to initate training
+
+    Params
+    ----------
+    df: dataframe
+    col: string, column to pull vocab from
+    VOCAB: array
+    baseEmbeddingDict: dict, used to lookup embeddings
+    isMultilabel: bool, can things belong in multiple classes?
+    labelCols: array, which col(s) are the target of prediction
+    ----------
+    return: tuple,
+            word_index=dictionary for embedding lookup,
+            embedding_weights=weights,
+            X_data=feature matrix,
+            labels=target of training,
+            ogText=the original text from 'col'
+    '''
     df[col]= df[col].astype(str)
     VOCAB_SIZE = len(VOCAB)
     tokenizer = Tokenizer(num_words=VOCAB_SIZE)
@@ -47,11 +84,9 @@ def process_embeddings(df, col, VOCAB, baseEmbeddingDict, isMultilabel, labelCol
     sequences = tokenizer.texts_to_sequences(df[col].tolist())
     labels = []
     word_index = tokenizer.word_index
-
     print('Found %s unique tokens.' % len(word_index))
 
     X_data = pad_sequences(sequences, maxlen=_config.EMBEDDINGS['MAX_SEQUENCE_LENGTH'])
-
     embedding_weights = np.zeros((len(word_index)+1, _config.EMBEDDINGS['EMBEDDING_DIM']))
     for word,index in word_index.items():
         embedding_weights[index,:] = baseEmbeddingDict[word] if word in baseEmbeddingDict else np.random.rand(_config.EMBEDDINGS['EMBEDDING_DIM'])
@@ -72,6 +107,17 @@ def process_embeddings(df, col, VOCAB, baseEmbeddingDict, isMultilabel, labelCol
 
 
 def data_split(x, y, VALIDATION_SPLIT):
+    '''
+    This splits the data to training set and testing set
+
+    Params
+    ----------
+    x: array, features
+    y: array, labels
+    VALIDATION_SPLIT: float, percent to allocate to test set
+    ----------
+    return: tuple, x_train, y_train, x_val, y_val
+    '''
     num_validation_samples = int(VALIDATION_SPLIT * x.shape[0])
     x_train = x[:-num_validation_samples]
     y_train = y[:-num_validation_samples]
@@ -82,9 +128,19 @@ def data_split(x, y, VALIDATION_SPLIT):
 
 
 def process_embeddings_inference(text, baseEmbeddingDict):
+    '''
+    This process text for inference
+
+    Params
+    ----------
+    text: string, the text to run inference on
+    baseEmbeddingDict: dict, to look up the embdding
+    ----------
+    return: array, to feed into classifer
+    '''
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(text)
     sequences = tokenizer.texts_to_sequences(text)
-    word_index = tokenizer.word_index  #need 2 return
+    word_index = tokenizer.word_index
     data = pad_sequences(sequences, maxlen=_config.EMBEDDINGS['MAX_SEQUENCE_LENGTH'])
     return data
